4b0444f634213f082f7ceb0d5eaa2a3549420cbe
Alex Sieke <asieke@gmail.com>
2019-09-30 15:26:48 -0700

    added basic scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { startGame, cancelGame } from '../actions/settings';
+import Instructions from './Instructions'
 
 class App extends Component{
 
-    render() {
-        return(
-            <div>
-                React App
-            </div>
-        )
-    }
+  render() {
+    console.log('this',this);
+      return(
+        <div>
+          <h2>♥ ♠ Evens or Odds ♠ ♥</h2>
+          {
+            this.props.gameStarted ? (
+              <div>
+                <h3>The game is on!</h3>
+                <br />
+                <button onClick={this.props.cancelGame}>Cancel Game</button>
+              </div>
+            ) : (
+              <div>
+                <h3>A new game awaits</h3>
+                <br />
+                <button onClick={this.props.startGame}>Start Game</button>
+                <hr />
+                <Instructions />
+              </div>                    
+            )
+          }
+        </div> 
+      )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { 
+    gameStarted: state.gameStarted 
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    startGame: () => dispatch(startGame()),
+    cancelGame: () => dispatch(cancelGame())
+  };
+}
+
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
+
+export default componentConnector(App);
---
+++ src/components/Instructions.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { expandInstructions, collapseInstructions } from '../actions/settings';
+
+const Instructions = props => {
+  const { instructionsExpanded, expandInstructions, collapseInstructions } = props;
+
+  if(instructionsExpanded){
+    return(
+      <div>
+        <button onClick={collapseInstructions}>Collapse Instructions</button>
+          <p>Click on a card</p>
+          <p>Try to guess!</p>
+      </div>      
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={expandInstructions}>Show Instructions</button>
+    </div> 
+  )
+}
+
+export default connect(
+  state => ({ instructionsExpanded: state.instructionsExpanded }),
+  { expandInstructions, collapseInstructions }
+)(Instructions);
